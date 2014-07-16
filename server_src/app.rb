ac8195ccd65ebba7898a24d0d6855bd36b3559b8
Luca Arcara <larcara@gmail.com>
2014-07-16 12:31:47 +0200

    app.rb

--- server_src/app.rb
+++ server_src/app.rb
@@ -1,12 +1,16 @@
 require "sinatra"
+require "sinatra/reloader" if development?
 require "xmlrpc/client"
 
 #API_KEY="TTU8rKwDFEN4uL4iuyO9wlNDTJE78kXR"
 API_KEY='bL5fwQZNDFdHkIR1hjxYCFIBrVRV78Hw'
-get '/authenticate' do |variable|
-	server = XMLRPC::Client.new("'http://muovi.roma.it/", "/ws/xml/", 80)
+get '/authenticate' do 
+	server = XMLRPC::Client.new_from_uri("http://muovi.roma.it/ws/xml/autenticazione/1")
+	server.http_header_extra = { 'Content-Type' => 'text/xml' }
+	server.http_header_extra = { 'Accept' => 'text/xml' }
+	puts "#{server}"
 	begin
-	  param = server.call("autenticazione.Accedi", API_KEY)
+	  param = server.call("autenticazione.Accedi", API_KEY, '')
 	  puts "#{param}"
 	rescue XMLRPC::FaultException => e
 	  puts "Error:"
